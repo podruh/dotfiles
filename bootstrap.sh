ac1c370821640a2fcdadfd10b7c23c2306e9c2ec
David Podroužek <podruh01@gmail.com>
2018-07-23 15:45:34 +0200

    Add powerlevel9k

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,10 @@ git pull origin master;
 
 function doIt() {
 	sudo apt install zsh
-	zsh
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+	git clone https://github.com/bhilburn/powerlevel9k.git  ~/.zprezto/modules/prompt/external/powerlevel9k
+	ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/prompt_powerlevel9k_setup
+	echo "Install powerline font https://github.com/bhilburn/powerlevel9k/wiki/Install-Instructions#step-2-install-a-powerline-font"
 	sudo chsh -s $(which zsh)	
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
@@ -15,6 +17,10 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "brew.sh" \
+		--exclude ".bash_profile" \
+		--exclude ".bash_prompt" \
+		--exclude ".bashrc" \
 		-avh --no-perms . ~;
 	 
 	source ~/.zshrc
