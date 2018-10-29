7a0477f6306262bb69f906d72be0b55dabd663bf
David Podroužek <podruh01@gmail.com>
2018-10-29 20:34:50 +0100

    Add git to zpretzto modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,15 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
+	echo Installing zsh...
 	sudo apt install zsh
+	echo installint zsh prezto...
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+ 	echo Installing  PowerLevel9k theme...
 	git clone https://github.com/bhilburn/powerlevel9k.git  ~/.zprezto/modules/prompt/external/powerlevel9k
 	ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/prompt_powerlevel9k_setup
 	echo "Install powerline font https://github.com/bhilburn/powerlevel9k/wiki/Install-Instructions#step-2-install-a-powerline-font"
-	sudo chsh -s $(which zsh)	
+	sudo chsh -s $(which zsh)
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
